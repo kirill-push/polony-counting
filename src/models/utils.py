260a4b6967048cd7a0a73413177bdecd0db82302
Kirill Pushkarev <kirillpush@yandex.ru>
2023-10-04 11:03:49 +0300

    style fixes

--- src/models/utils.py
+++ src/models/utils.py
@@ -6,9 +6,8 @@ import torch
 import wandb
 from torchvision import transforms
 
-from data.utils import read_tiff
+from data.utils import grid_to_squares, read_tiff
 from models.models import UNet
-from data.utils import grid_to_squares
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 FIRST_HORIZONTAL = 158
