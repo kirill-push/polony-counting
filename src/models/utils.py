25d1a51fea2ad1f13ca54a6f3212b2b9f93d6798
Kirill Pushkarev <kirillpush@yandex.ru>
2023-11-12 20:29:06 +0200

    style: :sparkles: minor fixes
    Unnecessary "else" after "return", removed "else"

--- src/models/utils.py
+++ src/models/utils.py
@@ -193,10 +193,9 @@ class Config(dict):
     def __getattr__(self, key):
         if key in self:
             return self[key]
-        else:
-            raise AttributeError(
-                f"'{type(self).__name__}' object has no attribute '{key}'"
-            )
+        raise AttributeError(
+            f"'{type(self).__name__}' object has no attribute '{key}'"
+        )
 
 
 def evaluate(
