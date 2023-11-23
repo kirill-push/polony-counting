706eafed1f8d57006e346594a78a53e649056fca
kirill-push <kirillpush@gmail.com>
2023-11-23 12:09:54 +0200

    style: :art: formatter fix

--- src/polony/data/utils.py
+++ src/polony/data/utils.py
@@ -44,7 +44,7 @@ def remove_img_without_roi(location: str, remove: bool = True) -> None:
             error_list.append(roi_path)
             continue
     if error_list:
-        errors_path = os.path.join(os.getcwd(), 'errors_path.txt')
+        errors_path = os.path.join(os.getcwd(), "errors_path.txt")
         with open(errors_path, "w") as file:
             for path in error_list:
                 file.write(path + "\n")
