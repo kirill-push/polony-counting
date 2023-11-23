4213782d26205aea3d9e450163d6aaf840ae3022
kirill-push <kirillpush@gmail.com>
2023-11-23 15:17:19 +0200

    style: :rotating_light: fix lint error

--- src/polony/models/__init__.py
+++ src/polony/models/__init__.py
@@ -1,4 +1,4 @@
 from . import predict_model as predict  # noqa: F401
-from .train_model import train  # noqa: F401
 from .models import UNet  # noqa: F401
+from .train_model import train  # noqa: F401
 from .utils import evaluate  # noqa: F401
