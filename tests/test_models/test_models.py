3029438e19204406363e3dbb64f3bd0133f60241
Kirill Pushkarev <71515921+kirill-push@users.noreply.github.com>
2023-11-26 18:26:52 +0200

    Revert "Feat/squares-classifier"

--- tests/test_models/test_models.py
+++ tests/test_models/test_models.py
@@ -1,7 +1,6 @@
 import torch
-import pytest
 
-from polony import UNet, Classifier
+from polony import UNet
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
@@ -20,17 +19,3 @@ def test_forward_pass_with_res():
     output = nn(input_data)
 
     assert output.shape == (1, 1, 316, 316)
-
-
-def test_classifier():
-    model = Classifier().to(device)
-    inp = torch.randn(size=(4, 3, 256, 256)).to(device)
-    model.freeze_layers('on')
-    assert model(inp).shape == (4, 1)
-    
-    model.freeze_layers('off')
-    assert model(inp).shape == (4, 1)
-
-    with pytest.raises(ValueError):
-        model.freeze_layers('onn')
-
