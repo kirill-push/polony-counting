23616f2c68d21d35d0a0f5a06d7aa8ded07ba77e
kirill-push <kirillpush@gmail.com>
2023-12-04 14:47:09 +0200

    test: :white_check_mark: update tests for predict function

--- tests/test_models/test_predict_model.py
+++ tests/test_models/test_predict_model.py
@@ -1,4 +1,8 @@
-from unittest.mock import patch
+from typing import Dict, List
+from unittest.mock import MagicMock, patch
+
+import pytest
+import torch
 
 from src.polony.models.predict_model import predict
 
@@ -42,3 +46,39 @@ def test_predict_directory() -> None:
                 assert result[0][1]["density"] == "density1"
                 assert result[1][2]["result"] == 20
                 assert result[1][2]["density"] == "density2"
+
+
+@pytest.fixture
+def mock_denisty_model():
+    mock = MagicMock()
+    mock_model_object = mock.return_value
+    mock_model_object.forward.return_value = torch.randn(1, 1, 256, 256)
+    mock_model_object.load_state_dict.return_value = mock_model_object
+    mock_model_object.module = mock_model_object
+    return mock
+
+
+class FakeModel(torch.nn.Module):
+    def forward(self, input):
+        return torch.randn(1, 1, 256, 256)
+
+    def load_state_dict(self, *args, **kwargs):
+        pass
+
+
+@patch("torch.nn.DataParallel")
+def test_predict_one_image_real_work(mock_dataparallel):
+    with patch("src.polony.models.predict_model.UNet", new=FakeModel) as mock_density:
+        with patch(
+            "src.polony.models.predict_model.Classifier", new=FakeModel
+        ) as mock_classifier:
+            mock_classifier.forward = lambda *args, **kwargs: torch.randn(1, 1)
+
+            mock_dataparallel.side_effect = lambda model: model
+            result = predict(
+                "resources/raw/test/test_img.tif",
+                density=mock_density(),
+                classifier=mock_classifier(),
+            )
+            assert isinstance(result, List)
+            assert isinstance(result[0], Dict)
