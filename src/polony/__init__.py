d13a9c131b443bf90a0f3d77eca2d305fa419a51
kirill-push <kirillpush@gmail.com>
2023-12-24 22:45:13 +0200

    chore: add to init new funcs to calculate phages

--- src/polony/__init__.py
+++ src/polony/__init__.py
@@ -1,5 +1,6 @@
 __version__ = "0.5.0"
 from .data import PolonyDataset, generate_polony_data, make_dataset
+from .efficacy import T4_efficacy_to_csv, T7_efficacy_to_csv
 from .models import Classifier, UNet, evaluate, predict, predict_one_image, train
 
 __all__ = [
@@ -12,4 +13,6 @@ __all__ = [
     "predict_one_image",
     "train",
     "Classifier",
+    "T4_efficacy_to_csv",
+    "T7_efficacy_to_csv",
 ]
