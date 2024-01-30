032269152f627d164507205646996eed6bef68eb
kirill-push <kirillpush@gmail.com>
2024-01-30 15:59:36 +0200

    chore: :zap: add general func to calculate efficacy for all virus types

--- src/polony/efficacy/polony_to_phage.py
+++ src/polony/efficacy/polony_to_phage.py
@@ -152,3 +152,34 @@ def T7c_efficacy_to_csv(
 
     # Writing the results to a new CSV file
     result.to_csv(output_path, index=False)
+
+
+def efficacy_to_csv(
+    virus_type: str,
+    input_path: str,
+    output_path: str,
+    alpha: float = 0.05,
+    n: int = 10000,
+) -> None:
+    """Function to calculate phages according to virus type from input file and write to
+        output file results.
+
+    Args:
+        virus_type (str): Type of virus. Can be T4, T7 or T7c.
+        input_path (str): Path to csv with polony countings.
+        output_path (str): Path to save results in csv file.
+        alpha (float, optional): Confidence level for the bootstrap interval.
+            Defaults to 0.05.
+        n (int, optional): Number of bootstrap samples.
+            Defaults to 10000.
+    """
+    if virus_type not in ["T4", "T7", "T7c"]:
+        raise ValueError("Virus type should be T4, T7 or T7c")
+    # Reading data from a CSV file
+    df = pd.read_csv(input_path)
+
+    # Applying the dat_for_plot function to the data
+    result = dat_for_plot(df, virus_type=virus_type, alpha=alpha, n=n)
+
+    # Writing the results to a new CSV file
+    result.to_csv(output_path, index=False)
